fc1818e0c86d5f90793be03c6b1b5713a837bc80
Cory Radel <cory.radel@gmail.com>
2012-06-27 01:12:12 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
+  resources :users
+
+  root to: 'static_pages#home'
+
+  match '/signup',  to: 'users#new'
 
   root to: 'static_pages#home'
 
